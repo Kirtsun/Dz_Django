dc05bd1367649d60ed348339569c571f5c16819b
Kirtsun <olegkirtsun@gmail.com>
2022-10-13 16:15:15 +0300

    init

--- myproject/settings.py
+++ myproject/settings.py
@@ -20,14 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-fa3^qtl=9w8tknn$@p+pv*xv*s*u84rl-@ivh=1!(@q)b$!%!f")
+SECRET_KEY = 'django-insecure-fa3^qtl=9w8tknn$@p+pv*xv*s*u84rl-@ivh=1!(@q)b$!%!f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "*"
-]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -39,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'catalog'
 ]
 
 MIDDLEWARE = [
