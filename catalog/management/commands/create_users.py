25cf9f732ce0218e25e4d4e8d331599c8362bf41
Oleg Kirtsun <olegkirtsun@gmail.com>
2022-10-25 22:07:27 +0300

    command

--- catalog/management/commands/create_users.py
+++ catalog/management/commands/create_users.py
@@ -1,12 +1,23 @@
 from django.core.management.base import BaseCommand
 from faker import Faker
+from catalog.models import User
+
+fake = Faker()
 
 
 class Command(BaseCommand):
     help = "init"
 
     def add_arguments(self, parser):
-        parser.add_argument('poll_ids', nargs='+', type=int)
+        parser.add_argument('some_id', nargs='+', type=int, choises=range(1, 10))
 
     def handle(self, *args, **options):
-        pass
+        some_id = options['some_id']
+        for i in range(some_id):
+            try:
+                some_id <= 10
+            except ValueError:
+                self.stdout.write('invalid value')
+            User.objects.create(username=fake.username(), email=fake.email(), password=fake.password())
+
+
