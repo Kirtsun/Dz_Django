89bde4e0d612860101daeb2880bc7c5989fde6f4
Oleg Kirtsun <olegkirtsun@gmail.com>
2022-10-26 18:34:48 +0300

    command delete users

--- catalog/management/commands/delet_users.py
+++ catalog/management/commands/delet_users.py
@@ -6,18 +6,13 @@ User = get_user_model()
 
 
 class Command(BaseCommand):
-    help = "Create random users"
+    help = "Delete users"
 
     def add_arguments(self, parser):
-        parser.add_argument('some_id', nargs='+', type=int, help='Enter a number from 1 to 10 to create random users')
+        parser.add_argument('some_id', nargs='+', type=int, help='Enter a number from 1')
 
     def handle(self, *args, **kwargs):
         users = kwargs['some_id']
+        a = User.objects.filter(pk=users[0])
+        print(a)
 
-        for i in users:
-            try:
-                user = User.objects.get(pk=i)
-                user.delete()
-                self.stdout.write('User "%s (%s)" deleted with success!' % (user.username, i))
-            except User.DoesNotExist:
-                self.stdout.write('User with id "%s" does not exist.' % i)
