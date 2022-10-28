b0eeeea901e5723ee96e9febb1f403e11b015a46
Oleg Kirtsun <olegkirtsun@gmail.com>
2022-10-28 19:23:54 +0300

    models

---
+++ catalog/models.py
@@ -0,0 +1,36 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class Citi(models.Model):
+    citi = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.citi
+
+
+class Client(models.Model):
+    citi = models.ForeignKey('Citi', on_delete=models.CASCADE)
+    goods = models.ManyToManyField('Goods')
+    names = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.names
+
+
+class Goods(models.Model):
+    products = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.products
+
+
+class Distributor(models.Model):
+    distrib = models.CharField(max_length=100)
+    citi = models.OneToOneField('Citi', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.distrib
+
