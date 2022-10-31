c7f3af0650fc7fe7474e6cdd3d41087aea75abc1
Oleg Kirtsun <olegkirtsun@gmail.com>
2022-11-01 00:45:06 +0200

    models

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "catalog"
 
-urlpatterns = [path('', views.index, name='index')]
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('triangle/', views.triangle, name='triangle')
+]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,24 @@
 
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+from .forms import Gipot
 
 
 def index(request):
     return HttpResponse("Hello. You're at the catalog index.")
+
+
+def triangle(request):
+    form = Gipot()
+    if 'Done' in request.GET:
+        form = Gipot(request.GET)
+        if form.is_valid():
+            leg1 = int(request.GET.get("leg1"))
+            leg2 = int(request.GET.get("leg2"))
+            print(type(leg1))
+            res = (leg1 ** 2 + leg2 ** 2) ** .5
+            return redirect(request, "catalog/triangle.html", {"res": res})
+    return render(request, "catalog/triangle.html", {"form": form})
+
+
