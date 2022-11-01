54e3e2f35a451b0eb44926aac034c848d7a2d3e7
Oleg Kirtsun <olegkirtsun@gmail.com>
2022-11-01 11:28:48 +0200

    models

--- catalog/views.py
+++ catalog/views.py
@@ -10,15 +10,16 @@ def index(request):
 
 
 def triangle(request):
-    form = Gipot()
     if 'Done' in request.GET:
         form = Gipot(request.GET)
         if form.is_valid():
             leg1 = int(request.GET.get("leg1"))
             leg2 = int(request.GET.get("leg2"))
-            print(type(leg1))
             res = (leg1 ** 2 + leg2 ** 2) ** .5
-            return redirect(request, "catalog/triangle.html", {"res": res})
-    return render(request, "catalog/triangle.html", {"form": form})
+            res = round(res, 2)
+            return render(request, "catalog/triangle.html", {'res': res})
+    else:
+        form = Gipot()
+        return render(request, "catalog/triangle.html", {"form": form})
 
 
